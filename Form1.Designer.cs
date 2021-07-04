163109e4a0a4c38b0b40e129506b5b60f356583e
nishimura <nishimura.abcba@gmail.com>
2021-07-04 17:16:45 +0900

    firstcommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace LogApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ステータス = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.todayTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ステータス,
+            this.time,
+            this.todayTotal,
+            this.comment});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(447, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ステータス
+            // 
+            this.ステータス.HeaderText = "status";
+            this.ステータス.Name = "ステータス";
+            // 
+            // time
+            // 
+            this.time.HeaderText = "time";
+            this.time.Name = "time";
+            // 
+            // todayTotal
+            // 
+            this.todayTotal.HeaderText = "todayTotal";
+            this.todayTotal.Name = "todayTotal";
+            // 
+            // comment
+            // 
+            this.comment.HeaderText = "comment";
+            this.comment.Name = "comment";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(66, 184);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "button1";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 304);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ステータス;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn todayTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn comment;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogApp
+{
+    public partial class Form1 : Form
+    {
+        private const string FOLDER_NAME = "Data";
+        private object obj = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ロードイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string todayData = string.Empty;
+            string filepath = string.Empty;
+            string path = @"C:\Users\Toshi\Desktop\new\Data\2021\07\04.txt";
+            // ファイルパスを取得する
+            filepath = fileCreate();
+            // CSVを読み込む
+            using (StreamReader reader = new StreamReader(filepath))
+            {
+                todayData = reader.ReadToEnd();
+            }
+ 
+            //現在の状況(計測中か否か)
+            //時間(今回の計測時間)
+            //合計時間(本日の合計計測時間)
+            //コメント(フリーフォーマット)
+
+        }
+
+        /// <summary>
+        /// 今日のファイルパスを取得する
+        /// </summary>
+        /// <returns>ファイルパス</returns>
+        private string fileCreate()
+        {
+            string folderPath = Path.Combine(Environment.CurrentDirectory, FOLDER_NAME);
+            string year = DateTime.Now.ToString("yyyy"); // 年
+            string manth = DateTime.Now.ToString("MM");  // 月
+            string day = DateTime.Now.ToString("dd") + ".txt";    // 日
+
+            // フォルダが存在しない場合は作成
+            if (!Directory.Exists(folderPath))
+            {
+                // フォルダーの作成
+                DirectoryCreate(folderPath);
+
+            }
+
+            // 現在の年のディレクトリ
+            string yearDirectory = Path.Combine(folderPath, year);
+            if (!Directory.Exists(yearDirectory))
+            {
+                // フォルダーの作成
+                DirectoryCreate(yearDirectory);
+            }
+
+            // 現在の月のディレクトリ
+            string manthDirectory = Path.Combine(yearDirectory, manth);
+            if (!Directory.Exists(manthDirectory))
+            {
+                // フォルダーの作成
+                DirectoryCreate(manthDirectory);
+            }
+            // ファイル読み込み
+            string filePath = Path.Combine(manthDirectory, day);
+            if (!File.Exists(filePath))
+            {
+                // ファイルの作成
+                File.Create(filePath).Close();
+
+                // アクセス権を変更する
+                FileSystemAccessRule rule = new FileSystemAccessRule(
+                  new NTAccount("everyone"),
+                  FileSystemRights.Modify,
+                  AccessControlType.Allow);
+
+                FileSecurity security = File.GetAccessControl(filePath);
+                security.AddAccessRule(rule);
+                File.SetAccessControl(filePath, security);
+            }
+            return filePath;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        /// <summary>
+        /// ディレクトを作成してアクセス権を付与する
+        /// </summary>
+        /// <param name="path">ディレクトリを作成するパス</param>
+        private void DirectoryCreate(string path)
+        {
+            Directory.CreateDirectory(path);
+
+            FileSystemAccessRule rule = new FileSystemAccessRule(
+              new NTAccount("everyone"),
+              FileSystemRights.FullControl,
+              InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit,
+              PropagationFlags.None,
+              AccessControlType.Allow);
+
+            DirectorySecurity security = Directory.GetAccessControl(path);
+            security.SetAccessRule(rule);
+            Directory.SetAccessControl(path, security);
+        }
+    }
+}
